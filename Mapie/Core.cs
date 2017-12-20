4115a07ab550bc73355e47c99d7cdd69991019ce
Nymann <s144833@student.dtu.dk>
2017-12-20 10:37:48 +0100

    Initial Commit

---
+++ Mapie/Core.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using PoeHUD.Models.Enums;
+using PoeHUD.Plugins;
+using PoeHUD.Poe.Components;
+using SharpDX;
+using SharpDX.Direct3D9;
+
+namespace Mapie
+{
+    public class Core : BaseSettingsPlugin<Settings>
+    {
+        public Core()
+        {
+            PluginName = "Mapie";
+        }
+
+        public override void Render()
+        {
+
+            if (!GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible)
+            {
+                return;
+            }
+
+            var inventory =
+                GameController.Game.IngameState.IngameUi.InventoryPanel[
+                    InventoryIndex.PlayerInventory];
+
+            var inventoryItems = inventory.VisibleInventoryItems;
+
+            foreach (var item in inventoryItems)
+            {
+                var mods = item.Item.GetComponent<Mods>().ItemMods;
+
+                foreach (var mod in mods)
+                {
+                    var modName = mod.Name.ToLower();
+
+                    if (Settings.ElementalReflecNode.Value && modName.Contains("reflect") && modName.Contains("elemental"))
+                    {
+                        Graphics.DrawFrame(item.GetClientRect(), 3, Color.Red);
+                        Graphics.DrawText("E-R", 25, item.GetClientRect().Center, Color.Red, FontDrawFlags.Center);
+                    }
+
+                    if (Settings.PhysicalReflecNode.Value && modName.Contains("reflect") && modName.Contains("physical"))
+                    {
+                        Graphics.DrawFrame(item.GetClientRect(), 3, Color.Brown);
+                        Graphics.DrawText("P-R", 25, item.GetClientRect().Center, Color.Brown, FontDrawFlags.Center);
+                    }
+
+                    if (Settings.TemporalChainsNode.Value && modName.Contains("temporal"))
+                    {
+                        Graphics.DrawFrame(item.GetClientRect(), 3, Color.Yellow);
+                        Graphics.DrawText("T-C", 25, item.GetClientRect().Center, Color.Yellow, FontDrawFlags.Center);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Mapie/Settings.cs
@@ -0,0 +1,30 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+
+namespace Mapie
+{
+    public class Settings : SettingsBase
+    {
+        public Settings()
+        {
+            Enable = false;
+
+            SettingsNode = new EmptyNode();
+            ElementalReflecNode = true;
+            PhysicalReflecNode = true;
+            TemporalChainsNode = true;
+        }
+
+        [Menu("Settings", 7777)]
+        public EmptyNode SettingsNode { get; set; }
+
+        [Menu("Elemental Reflect", "Enable this, if you want to be warned when a map has the mod: \"Elemental Reflect\".", 7770, 7777)]
+        public ToggleNode ElementalReflecNode { get; set; }
+
+        [Menu("Physical Reflect", "Enable this, if you want to be warned when a map has the mod: \"Physical Reflect\".", 8880, 7777)]
+        public ToggleNode PhysicalReflecNode { get; set; }
+
+        [Menu("Temporal Chains", "Enable this, if you want to be warned when a map has the mod: \"Temporal Chains\".", 9990, 7777)]
+        public ToggleNode TemporalChainsNode { get; set; }
+    }
+}
